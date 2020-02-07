5f884023c8df974a1080a087ce11935e8700da89
charles <charles.stolive@dayonmars.fr>
2020-02-07 01:13:55 +0100

    Modifs

--- src/assets/javascript/app.js
+++ src/assets/javascript/app.js
@@ -7,11 +7,17 @@ window.$ = $;
 /*
 * Application
 */
-console.log($)
+var mobile;
+$(window).resize(function () {
+    mobile = window.matchMedia("(max-width: 1024px)").matches;
+    console.log(mobile);
+}).resize();
 
 var scrollpos = window.scrollY;
 var header = document.getElementById("header");
 var navcontent = document.getElementById("nav-content");
+var btnMobile = document.getElementById("btn_mobile");
+
 var logo = document.getElementById("logo_talktob");
 var navaction = document.getElementById("navAction");
 var brandname = document.getElementById("brandname");
@@ -22,23 +28,22 @@ var toAnimate = document.querySelectorAll(".toAnimate");
 for (var i = 0; i < toAnimate.length; i++) {
     toAnimate[i].classList.add('hidden');
 }
+changeAppparences(0);
 
 document.addEventListener('scroll', function () {
 
     /*Apply classes for slide in bar*/
     scrollpos = window.scrollY;
+    changeAppparences(scrollpos);
 
-    if (scrollpos > 10) {
+
+});
+
+function changeAppparences(scroll) {
+    if (scrollpos > 10 || mobile) {
         header.classList.add("bg-white");
         navaction.classList.remove("bg-secondary");
         navaction.classList.add("bg-primary");
-        // logo.classList.remove('fill-secondary');
-        // logo.classList.remove('stroke-secondary');
-        // logo.classList.add('fill-primary');
-        // logo.classList.add('stroke-primary');
-
-        // navaction.classList.remove("text-gray-800");
-        // navaction.classList.add("text-white");
         //Use to switch toggleColour colours
         for (var i = 0; i < toToggle.length; i++) {
             toToggle[i].classList.add("text-gray-800");
@@ -46,7 +51,6 @@ document.addEventListener('scroll', function () {
         }
         for (var i = 0; i < toAnimate.length; i++) {
             var anime = isAnyPartOfElementInViewport(toAnimate[i]);
-            console.log(anime);
             if (anime) {
                 toAnimate[i].classList.remove('hidden');
                 toAnimate[i].classList.add('animated');
@@ -68,13 +72,7 @@ document.addEventListener('scroll', function () {
         header.classList.remove("bg-white");
         navaction.classList.remove("bg-primary");
         navaction.classList.add("bg-secondary");
-        // logo.classList.add('fill-secondary');
-        // logo.classList.add('stroke-secondary');
-        // logo.classList.remove('fill-primary');
-        // logo.classList.remove('stroke-primary');
-        // navaction.classList.remove("text-gray-800");
-        // navaction.classList.add("text-white");
-        //Use to switch toggleColour colours
+        //
         for (var i = 0; i < toToggle.length; i++) {
             toToggle[i].classList.add("text-white");
             toToggle[i].classList.remove("text-gray-800");
@@ -85,25 +83,26 @@ document.addEventListener('scroll', function () {
             logoToToggle[i].classList.remove("fill-primary");
             logoToToggle[i].classList.remove("stroke-primary");
         }
-
+        //
         header.classList.remove("shadow");
         navcontent.classList.remove("bg-white");
         navcontent.classList.add("bg-gray-100");
 
     }
-
-});
+}
 
 /*Toggle dropdown list*/
 /*https://gist.github.com/slavapas/593e8e50cf4cc16ac972afcbad4f70c8*/
 
 var navMenuDiv = document.getElementById("nav-content");
 var navMenu = document.getElementById("nav-toggle");
+//
+var navNeedDiv = document.getElementById("nav-need-content");
+var needMenu = document.getElementById("nav-need");
 
 document.onclick = check;
 function check(e) {
     var target = (e && e.target) || (event && event.srcElement);
-
     //Nav Menu
     if (!checkParent(target, navMenuDiv)) {
         // click NOT on the menu
@@ -115,6 +114,15 @@ function check(e) {
         } else {
             // click both outside link and outside menu, hide menu
             navMenuDiv.classList.add("hidden");
+            navNeedDiv.classList.add("hidden");
+        }
+    } else {
+        if (checkParent(target, needMenu)) {
+            if (navNeedDiv.classList.contains("hidden")) {
+                navNeedDiv.classList.remove("hidden");
+            } else {
+                navNeedDiv.classList.add("hidden");
+            }
         }
     }
 
